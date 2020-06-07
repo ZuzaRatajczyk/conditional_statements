4724adfda17638cf6cb4a9a23bf38b3071fbac63
Zuza Ratajczyk <zuza.ratajczyk@gmail.com>
2020-06-07 18:52:48 +0200

    List.sort, correct ratio

--- exercise_5.py
+++ exercise_5.py
@@ -1,28 +1,20 @@
-a = int(input("Podaj długość boku "))
-b = int(input("Podaj długość boku "))
-c = int(input("Podaj długość boku "))
+a = int(input("Podaj długość boku: "))
+b = int(input("Podaj długość boku: "))
+c = int(input("Podaj długość boku: "))
+
+tringle = [a,b,c]
+tringle.sort() #sortuje liczby od najmniejszej do największej
+a = tringle[0]
+b = tringle[1]
+c = tringle[2]
 
 if a + b > c and a + c > b and b + c > a:
     print("Można utworzyć trójkąt")
-    if b < a > c:
-        a = c
-        c = a
-        if a**2 + b**2== c**2:
-            print("Trójkąt pitagorejski")
-            if a/b/c == 3/4/5:
-                print("Trójkąt egipski")
-    if a < b > c:
-        b = c
-        c = b
-        if a ** 2 + b ** 2 == c ** 2:
-            print("Trójkąt pitagorejski")
-            if a/b/c == 3/4/5:
-                print("Trójkąt egipski")
-    if a ** 2 + b ** 2 == c ** 2:
+    if a**2 + b**2 == c**2:
         print("Trójkąt pitagorejski")
-        if a / b / c == 3 / 4 / 5:
+        if a/3 == b/4 == c/5:
             print("Trójkąt egipski")
     else:
         print("Trójkąt niepitagorejski")
 else:
-    print("Nie można utworzyć trójkąta")
+    print("Nie można utworzyć trójkątu")
